2aeae4b9a5cd077298fbf29b76e984247860b3ed
Krishna Sumanth Pulikonda <krishnasumanth@usf.edu>
2024-02-27 15:12:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace New_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
